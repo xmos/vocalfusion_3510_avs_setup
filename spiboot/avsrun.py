8c7ef4cd057a1c8983e09dfea3e146ed564bacfb
shuchitak <shuchitak@xmos.com>
2019-08-08 09:57:32 +0100

    cleaned up ctrl+c triggered cleanup code

--- spiboot/avsrun.py
+++ spiboot/avsrun.py
@@ -11,41 +11,29 @@ import time
 import shutil
 
 spi_boot_in_progress = True
-def cleanup():
-    global spi_boot_in_progress
-    spi_boot_in_progress = False
-    try:
-        subprocess.call(['killall', 'SampleApp'])
-    except:
-        pass
-    sys.exit()
-
 
 def led_function():
-    try:
-        global spi_boot_in_progress
-        while spi_boot_in_progress:
-            subprocess.call(["./pi_hat_ctrl", "SET_LED_RGB", "22", "1", "1"])
-        subprocess.call(["./pi_hat_ctrl", "SET_LED_RGB", "19", "23", "3"])
-    except KeyboardInterrupt:#possibly not needed
-        pass
+    global spi_boot_in_progress
+    while spi_boot_in_progress:
+        subprocess.call(["./pi_hat_ctrl", "SET_LED_RGB", "22", "1", "1"])
+    subprocess.call(["./pi_hat_ctrl", "SET_LED_RGB", "19", "23", "3"])
     return
 
 def do_spiboot():
     try:
         with open(os.devnull, 'w') as devnull:
-            silence_process = subprocess.Popen(['aplay', '-c', '2', '-f', 'S16_LE', '-r', '48000', '/dev/zero'], stderr=devnull)
-            spiboot_process = subprocess.Popen(["python", "/home/pi/vocalfusion_3510_avs_setup/spiboot/send_image_from_rpi.py", "/home/pi/vocalfusion_3510_avs_setup/spiboot/app_xk_xvf3510_l71_i2s_slave_spi_slave.bin"], stderr=devnull, stdout=devnull)
+            silence_process = subprocess.Popen(['aplay', '-c', '2', '-f', 'S16_LE', '-r', '48000', '/dev/zero'])
+            spiboot_process = subprocess.Popen(["python", "send_image_from_rpi.py", "app_xk_xvf3510_l71_i2s_slave_spi_slave.bin"])
 
         spiboot_process.wait() #wait for spiboot to finish
         silence_process.terminate() #stop aplay
     except KeyboardInterrupt:
         spiboot_process.kill()
-        time.sleep(2)
         silence_process.kill()
+        print('do spiboot caught a ctrl+c')
+        raise KeyboardInterrupt('do_spiboot caught a ctrl+c')
         #instead of returning error raise KeyboardInterrupt
-        return -1
-    return 0
+    return
 
 def run_avs():
     try:
@@ -61,9 +49,7 @@ def run_avs():
                 pass
 
         led.start()
-        ret = do_spiboot()
-        if ret != 0:
-            cleanup()
+        do_spiboot()
 
         while True:
             try:
@@ -88,15 +74,14 @@ def run_avs():
                     avs.kill() #set avs to None
                     led.start()
                 
-                ret = do_spiboot()
-                if ret != 0:
-                    cleanup()
+                do_spiboot()
 
             if spi_boot_in_progress == False:
                 time.sleep(3)
             pass
     except KeyboardInterrupt:
         try:
+            print('run_avs caught a ctrl+c')
             spi_boot_in_progress = False #this will make led process exit
             time.sleep(1)
             subprocess.call(['killall', 'SampleApp']) #kill avs process
@@ -106,6 +91,4 @@ def run_avs():
 
 
 if __name__ == "__main__":
-    #just call run_avs function
-    p = Process(target=run_avs)
-    p.start()
+    run_avs()
